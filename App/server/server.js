329a2a474218cc09116d6e9c08cf58fc02368ab8
SoulReaper0123 <louisandredysarino@gmail.com>
2025-10-24 14:44:45 +0800

    option

--- App/server/server.js
+++ App/server/server.js
@@ -48,7 +48,13 @@ app.use(cors({
 }));
 
 // Handle preflight requests
-app.options('*', cors());
+// Handle preflight requests for all routes
+app.options('/*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'https://fiveki.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.status(204).send();
+});
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
